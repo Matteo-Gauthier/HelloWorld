4c925fadbf75cbef6f30e662ac3a1fc909f2b367
Matteo-Gauthier <mgaut185@uottawa.ca>
2023-10-23 10:20:25 -0400

    Added comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,14 @@
+/**
+* HelloWorld --- a program that prints "Hello World!".
+* @author Matteo Gauthier
+*/
 public class HelloWorld {
+
+  /**
+  * main function --- prints "Hello World!".
+  */
   public static void main(String[] args) {
-    //Change
     System.out.println("Hello World!");
-  }
-}
+  } //End main function
+
+} //End HelloWorld class
